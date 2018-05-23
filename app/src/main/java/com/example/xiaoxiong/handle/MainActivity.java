18523ed573143f3bb2693a6ed8c61b25bb934cb9
sugobanana <annahana@163.com>
2018-05-23 13:07:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xiaoxiong/handle/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.xiaoxiong.handle;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    public static final int UPDATE_TEXT = 1;
+    private TextView text;
+    private Handler handler = new Handler(){
+        public void handleMessage(Message msg){
+            switch (msg.what){
+                case UPDATE_TEXT:
+                    text.setText("你好，世界！");
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.change:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Message message = new Message();
+                        message.what = UPDATE_TEXT;
+                        handler.sendMessage(message);
+                    }
+                }).start();
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView)findViewById(R.id.text);
+        Button change = (Button)findViewById(R.id.change);
+        change.setOnClickListener(this);
+    }
+
+
+}
